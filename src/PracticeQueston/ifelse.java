62203ee9299b5c480473310458133943c4aaf40a
priyanshu <priyanshusengarps@gmail.com>
2024-09-01 11:03:35 +0530

    First Program

--- src/PracticeQueston/ifelse.java
+++ src/PracticeQueston/ifelse.java
@@ -2,6 +2,6 @@ package PracticeQueston;
 
 public class ifelse {
     public static void main(String[] args) {
-        System.out.println("JAVA");
+        System.out.println("Luckey Sengar");
     }
 }
